f98f2af5baa1d5fb95e91b55a991dedf9d621f0c
Jonathan Deissler <1810929@stud.hs-mannheim.de>
2022-11-13 15:56:30 +0100

    4.1 bis aufgabe c

--- Blatt4/Blatt4.py
+++ Blatt4/Blatt4.py
@@ -1,1 +1,21 @@
 from tensorflow import keras
+import numpy as np
+from nnwplot import plotTwoFeatures
+from matplotlib import pyplot as plt
+
+model = keras.models.Sequential()
+
+model.add(keras.layers.Dense(units=3, activation="softmax", input_shape=(2,), name="output_layer"))
+
+model.compile(optimizer="adam", loss="mean_squared_error", metrics=["accuracy"])
+
+iris = np.loadtxt(fname="iris.csv", delimiter=",")
+
+X = iris[:, :-1]
+T = iris[:, -1]
+X = X[:, 2:]
+print(f"x shape {X.shape},  T shape {T.shape}")
+model.fit(x=X,y=keras.utils.to_categorical(T),epochs=100)
+
+plotTwoFeatures(X,T,model.predict)
+
